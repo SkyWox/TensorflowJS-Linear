0ff5b4fee961d3eb07eb81503d3a8d5274ea9d22
Skylar Wilcox <skylardwilcox@gmail.com>
2018-05-01 20:42:00 -0700

    move from strings to ints

--- data.js
+++ data.js
@@ -12,10 +12,10 @@ export function generateData(numPoints, coeff, sigma = 0.04) {
     // Generate polynomial data - can't add sequentially - has to be one go
     var ys = tf
       .randomNormal([numPoints], 0, sigma) // Add random noise to the generated data
-      .add(coe['0'].mul(xs.pow(tf.scalar(0, 'int32'))))
-      .add(coe['3'].mul(xs.pow(tf.scalar(3, 'int32'))))
-      .add(coe['2'].mul(xs.pow(tf.scalar(2, 'int32'))))
-      .add(coe['1'].mul(xs.pow(tf.scalar(1, 'int32'))))
+      .add(coe[0].mul(xs.pow(tf.scalar(0, 'int32'))))
+      .add(coe[3].mul(xs.pow(tf.scalar(3, 'int32'))))
+      .add(coe[2].mul(xs.pow(tf.scalar(2, 'int32'))))
+      .add(coe[1].mul(xs.pow(tf.scalar(1, 'int32'))))
 
     // Normalize the y values to the range 0 to 1.
     const ymin = ys.min()
